257b1950a40111c2bf9f52c6f7ae6c776b0601f9
AAK581 <adhamahmad@outlook.com>
2025-08-08 22:34:49 +0300

    api issue

--- api/setKittens.js
+++ api/setKittens.js
@@ -1,13 +1,8 @@
 const ethers = require("ethers");
 
 export default async function handler(req, res) {
-res.setHeader('Access-Control-Allow-Origin', req.headers.origin || 'https://monad-cats-game.vercel.app');
-res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  if (req.method === 'OPTIONS') {
-  res.status(200).end();
-  return;
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Temporary CORS fix
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
   
   try {
     console.log("setKittens API: Checking GAME_PRIVATE_KEY", { hasKey: !!process.env.GAME_PRIVATE_KEY });
