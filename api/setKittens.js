868f142f1b1ebd0e82d2e60ae543702d5b700092
AAK581 <adhamahmad@outlook.com>
2025-08-08 22:26:38 +0300

    api issue

--- api/setKittens.js
+++ api/setKittens.js
@@ -1,9 +1,9 @@
 const ethers = require("ethers");
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Origin', req.headers.origin || 'https://monad-cats-game.vercel.app');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
   res.status(200).end();
   return;
