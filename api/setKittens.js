4e83e812510c6341e54855d66d71feaff70cf365
AAK581 <adhamahmad@outlook.com>
2025-08-08 23:03:52 +0300

    api fix

--- api/setKittens.js
+++ api/setKittens.js
@@ -2,7 +2,7 @@ const ethers = require("ethers");
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Use for testing, restrict later
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS, GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
     res.status(200).end();
